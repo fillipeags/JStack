31d67d60b34b4e712225b78ca68780b383752daa
luizgallas <luiz.gallas@compasso.com.br>
2021-10-27 12:24:24 -0300

    Adiciomado Error Handler do express no projeto

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,3 +1,4 @@
+require('express-async-errors');
 const express = require('express');
 const routes = require('./routes');
 
@@ -5,5 +6,10 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => { // Error handler
+  console.log('Error handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
