a38c1e21ab635d5e20eb6c08fe61621fb8e52cbd
Luiz Gallas <luizgallas@outlook.com>
2022-04-28 14:14:34 -0300

    Tratamento de erros

--- mycontacts/web/src/components/ContactForm/index.js
+++ mycontacts/web/src/components/ContactForm/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import PropTypes from 'prop-types';
 
 import Button from '../Button';
@@ -7,24 +8,81 @@ import Input from '../Input';
 import FormGroup from '../FormGroup';
 
 import { Form, ButtonContainer } from './styles';
+import isEmailValid from '../../utils/isEmailValid';
+import useErrors from '../../hooks/useErrors';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+  const {
+    setError, removeError, getErrorMessageByFieldName,
+  } = useErrors();
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
+  function handleEmailChange(event) {
+    const eventValue = event.target.value;
+
+    setEmail(eventValue);
+
+    if (eventValue && !isEmailValid(eventValue)) {
+      setError({ field: 'email', message: 'Email inválido' });
+    } else {
+      removeError('email');
+    }
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup error={getErrorMessageByFieldName('name')}>
+        <Input
+          error={getErrorMessageByFieldName('name')}
+          value={name}
+          placeholder="Nome"
+          onChange={handleNameChange}
+        />
       </FormGroup>
-      <FormGroup
-        error="O formato do email é inválido"
-      >
-        <Input error placeholder="Email" />
+      <FormGroup error={getErrorMessageByFieldName('email')}>
+        <Input
+          error={getErrorMessageByFieldName('email')}
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Email"
+        />
       </FormGroup>
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          value={phone}
+          placeholder="Telefone"
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
-          <option value="Instagram">Instagram</option>
+        <Select onChange={(event) => setCategory(event.target.value)}>
+          <option value="">Selecione</option>
+          <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
